20372d7b0daf86434007d870374c293938dfaa28
JiyaYUN <142545110+JiyaYUN@users.noreply.github.com>
2023-11-27 17:14:50 -0500

    Add files via upload

---
+++ Cobblestone Learning Center Case 2.R
@@ -0,0 +1,50 @@
+remove(list = ls())
+# Import Databases
+Mark <- read.csv("D:/Simon.UR/Fall B/Analytics Design and Application/evals_experiment.csv")
+
+Student <- read.csv("D:/Simon.UR/Fall B/Analytics Design and Application/students_experiment.csv")
+
+Mark <- merge(Mark, Student, by="student_id")
+
+summary(Mark)
+unique(Mark$district)
+
+for (i in 1:nrow(Mark)) {
+     if ("St. Paul" %in% Mark$district[i]) {
+          Mark$EducationalProgram[i] <- "Current"
+     } else if ("Beaverton" %in% Mark$district[i]) {
+          Mark$EducationalProgram[i] <- "Current"
+     } else if ("Lake Oswego" %in% Mark$district[i]) {
+          Mark$EducationalProgram[i] <- "A"
+     } else if ("Ridgefield" %in% Mark$district[i]) {
+          Mark$EducationalProgram[i] <- "A"
+     } else if ("Camas" %in% Mark$district[i]) {
+          Mark$EducationalProgram[i] <- "B"
+     } else if ("Sherwood" %in% Mark$district[i]) {
+          Mark$EducationalProgram[i] <- "B"
+     } else if ("Riverdale" %in% Mark$district[i]) {
+          Mark$EducationalProgram[i] <- "C"
+     } else {
+          Mark$EducationalProgram[i] <- "C"
+     }
+}
+
+library(dplyr)
+
+MarkDiff <- function(data) {
+     data <- data %>%
+          mutate(
+               score_reading = ifelse(lag(student_id) == student_id, score_reading - lag(score_reading), NA),
+               score_writing = ifelse(lag(student_id) == student_id, score_writing - lag(score_writing), NA),
+               score_mathNoCalc = ifelse(lag(student_id) == student_id, score_mathNoCalc - lag(score_mathNoCalc), NA),
+               score_mathCalc = ifelse(lag(student_id) == student_id, score_mathCalc - lag(score_mathCalc), NA),
+               score_total = score_reading + score_writing + score_mathNoCalc + score_mathCalc
+          )
+     
+     return(data)
+}
+Mark <- MarkDiff(Mark)
+Mark <- Mark %>%
+     filter(program != "intake")
+
+summary(lm())
